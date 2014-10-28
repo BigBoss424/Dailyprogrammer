8666437e3b99d90f6c6ff955e7859c773815f139
bigboss424 <ajones061392@gmail.com>
2014-10-27 20:16:23 -0700

    Added the class and .java file for the exercise. I believe it will be easier to accomplish this task using bash scripting. I'll be posting the solution for the problem in the languages that other people used to see how they work.

---
+++ Monday/102714/Admin.java
@@ -0,0 +1,39 @@
+/*
+Created by: Aaron Jones
+Description: This program will do all the things on the required tasklist on the programming challenge at: http://www.reddit.com/r/dailyprogrammer/comments/2kh2tz/10272014_challenge_186_easy_admin_schmadmin/. 
+
+First task on the list is:
+
+1) Get the 20 last used documents from the system and sort by the date they were modified.
+
+Second task on the list is: 
+
+2) Output the above command to a .txt file. 
+
+Third task on the list is:
+
+3) Retrieve the last 10 commands used on the console.
+
+Fourth task on the list is: 
+
+4) Get the 10 most CPU-heavy processes in descending order.
+
+Fifth task on the list is: 
+
+5) Retrieve the last 20 error logs/messages and output these as a formatted HTML table 
+
+Sixth task on the list is: 
+
+6) Retrieve all txt/pdf/exe files on the machine (You do not need to do the whole machine, just 1 drive is enough, or less if your machine is struggling).
+
+
+*/
+
+public class Admin
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Think I'm going to do this 					 one via bash script.");
+	}//end main
+
+}//end class
