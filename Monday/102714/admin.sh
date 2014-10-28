ac061cddb25f6b14db6e2b4eaa9a298dd2a42975
bigboss424 <ajones061392@gmail.com>
2014-10-27 20:24:46 -0700

    Started the construction of the daily programmer exercise using shell scripting.

---
+++ Monday/102714/admin.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#Created by: Aaron Jones
+#Description: This program will do all the things on the required tasklist on 
+#the programming challenge at: 
+#http://www.reddit.com/r/dailyprogrammer/comments/2kh2tz/10272014_challenge_186_easy_admin_schmadmin/. 
+
+#First task on the list is:
+
+#1) Get the 20 last used documents from the system and sort by the date they were modified.
+
+#Second task on the list is: 
+
+#2) Output the above command to a .txt file. 
+
+#Third task on the list is:
+
+#3) Retrieve the last 10 commands used on the console.
+
+#Fourth task on the list is: 
+
+#4) Get the 10 most CPU-heavy processes in descending order.
+
+#Fifth task on the list is: 
+
+#5) Retrieve the last 20 error logs/messages and output these as a formatted HTML table 
+
+#Sixth task on the list is: 
+
+#6) Retrieve all txt/pdf/exe files on the machine (You do not need to do the whole machine, just 1 drive is enough, or less if your machine is struggling).
+
+echo -e "\n"
+echo "==============================================================="
+echo "First task on the list"
+echo "1) Get the 20 last used documents from" 
+echo "the system and sort by the date they were modified."
+echo "==============================================================="
+echo -e "\n"
+
+echo -e "\n"
+echo "==============================================================="
+echo "Second task on the list"
+echo "2) Output the above command to a .txt file. " 
+echo "==============================================================="
+echo -e "\n"
+
+echo -e "\n"
+echo "==============================================================="
+echo "Third task on the list"
+echo "3) Retrieve the last 10 commands used on the console." 
+echo "==============================================================="
+echo -e "\n"
+
+echo -e "\n"
+echo "==============================================================="
+echo "Fourth task on the list"
+echo "4) Get the 10 most CPU-heavy processes in descending order." 
+echo "==============================================================="
+echo -e "\n"
+
+echo -e "\n"
+echo "==============================================================="
+echo "Fifth task on the list"
+echo "5) Retrieve the last 20 error logs/messages and output these" 
+echo "as a formatted HTML table"
+echo "==============================================================="
+echo -e "\n"
+
+echo -e "\n"
+echo "==============================================================="
+echo "Sixth task on the list"
+echo "6)Retrieve all txt/pdf/exe files on the machine (You do not need"
+echo "to do the whole machine, just 1 drive is enough, or less if your" 
+echo "machine is struggling)."
+echo "==============================================================="
+echo -e "\n"
