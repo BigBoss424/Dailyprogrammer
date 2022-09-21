1539cf7de7724d453621a5ec8eb03be9d68030e0
Aaron Jones <ajones061392@yahoo.com>
2022-09-20 18:44:04 -0700

    I couldn't make a triangle.

--- 2022/august/3/C/triangle.c
+++ 2022/august/3/C/triangle.c
@@ -15,36 +15,5 @@ int main()
     printf("He was then %d years old.\n", characterAge);
     printf("My favorite %s is %f", "number", 500.98754);
 
-    printf("Here is Pascal's Triangle");
-    int n;
-    printf("Enter the amount of rows: %s", n);
-
-    for(int i = 0; i <= n; i++){
-        for(int j = 0; j<= n-i; j++){
-            printf(" ");
-        }
-        for(int j = 0; j <= i; j++){
-            printf("%2d", ncr(i, j));
-        }
-        printf("\n");
-    }
-    
     return 0;
 }
-
-int pascalsTriangle(int n){
-    int f; 
-
-    for(f = 1; n > 1; n--)
-    {
-        f *= n;
-    }
-
-    return f;
-}
-
-int ncr(int n, int r){
-    return factorial(n) / (factorial(n-r) * factorial(r));
-
-}
-
